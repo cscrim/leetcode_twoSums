2aeeb951b0a225fabbf26ae6e27c27e0e2718b13
Courtney S <courtneyscrimshaw@gmail.com>
2024-10-22 18:24:35 -0400

    second solution with o(n) time complexity

--- twosums.js
+++ twosums.js
@@ -4,6 +4,33 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 
+// better solution with O(n) time complexity
+
+function twoSums(nums, targ) {
+    const numberMap = new Map();
+
+    for (x=0; x<nums.length; x++) {
+        const matchValue = targ - nums[x]
+
+        if (numberMap.has(matchValue)){
+            return [numberMap.get(matchValue), x]
+        }
+        
+        numberMap.set(nums[x], x);
+    }
+
+    return [];
+}
+
+const nums = [3, 9, 14, 23];
+targ = 37;
+
+console.log(twoSums(nums, targ));
+
+
+
+// first solution with O(n^2) time complexity
+
 function twoSum(num, target) {
 
     for (i=0; i<num.length; i++) {
